c041e6d0d986a53d4a3b94222bb51d94b55346d8
Sumaia <Somaiya.ghoraibi@gmail.com>
2026-02-21 13:39:33 +0300

    add statment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 
 
-    public class HelloWorld_git {
+    public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, SE4111!");
         System.out.println("Welcome to Software Construction");
+        System.out.println("Second Lab");
+
             }
 }
     

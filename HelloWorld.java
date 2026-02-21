1df8e9b64a9493a9b0cfcd367f8ad42b00905e43
Sumaia <Somaiya.ghoraibi@gmail.com>
2026-02-21 13:36:01 +0300

    add file

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+
+    public class HelloWorld_git {
+    public static void main(String[] args) {
+        System.out.println("Hello, SE4111!");
+        System.out.println("Welcome to Software Construction");
+            }
+}
+    
+
